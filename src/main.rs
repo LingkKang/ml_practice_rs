4a4ff5536d41c06cbf54bcdf4bf25f5ae95583ab
Lingkang <karlzhu12@gmail.com>
2023-05-07 22:08:24 +0800

    Implement BFS

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,47 @@
+use std::collections::VecDeque;
+
 mod tram;
 
+type Path = Vec<tram::Action>;
+type Address = u64;
+type Cost = u64;
+
 fn main() {
-    let my_instance = tram::TransportationProblem::new(5);
-    let b = my_instance.is_end(4);
-    println!("{}", b);
-    println!("{:?}", my_instance.next_act_cost(1));
+    let my_instance = tram::TransportationProblem::new(100);
+    let path: Path;
+    let cost: Cost;
+    (path, cost) = breadth_first_search(my_instance);
+    println!("Find pash is {:?}", path);
+    println!("With time cost {:?}", cost);
+}
+
+/// implement breadth first search for tram problem
+fn breadth_first_search(problem: tram::TransportationProblem) -> (Path, Cost) {
+    let mut que: VecDeque<(Address, Cost, Path)> = VecDeque::new();
+    que.push_back((problem.start(), 0, Vec::new()));
+
+    let mut action: tram::Action;
+    let mut new_state: Address;
+    let mut cost: Cost;
+    let mut history_cost: Cost;
+    let mut path: Path;
+
+    loop {
+        let item = que.pop_front();
+        match item {
+            Some(x) => (new_state, history_cost, path) = x,
+            None => {
+                panic!("Unreachable destination!");
+            }
+        }
+        if problem.is_end(new_state) {
+            return (path, history_cost);
+        }
+        for item in problem.next_act_cost(new_state) {
+            (action, new_state, cost) = item;
+            let mut new_path = path.clone();
+            new_path.push(action);
+            que.push_back((new_state, history_cost + cost, new_path));
+        }
+    }
 }
--- src/tram.rs
+++ src/tram.rs
@@ -1,26 +1,36 @@
+type Address = u64;
+type Cost = u64;
 
+/// Simply transport from `start` to `dest`
+/// with possible actions defined in `Action`
 pub struct TransportationProblem {
-    start: u64,
-    dest: u64,
+    start: Address,
+    dest: Address,
 }
 
-#[derive(Debug)]
-
+/// Available action in transportation problem
+/// - `Walk` take you from `k` to `k + 1` with time cost `1`
+/// - `Tram` take you from `k` to `2 * k` with time cost `2`
+#[derive(Debug, Clone)]
 pub enum Action {
     Walk,
     Tram,
 }
 
 impl TransportationProblem {
-    pub fn new(dest: u64) -> Self {
+    pub fn new(dest: Address) -> Self {
         Self { start: 1, dest }
     }
 
-    pub fn is_end(&self, state: u64) -> bool {
+    pub fn start(&self) -> Address {
+        self.start
+    }
+
+    pub fn is_end(&self, state: Address) -> bool {
         self.dest == state
     }
 
-    pub fn next_act_cost(&self, state: u64) -> Vec<(Action, u64, u64)> {
+    pub fn next_act_cost(&self, state: Address) -> Vec<(Action, Address, Cost)> {
         let mut ans = Vec::new();
         if state + 1 <= self.dest {
             ans.push((Action::Walk, state + 1, 1));
