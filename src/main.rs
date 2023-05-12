a51d8127ae56cbe8e9db5fdb30f376b03f9405cd
Lingkang <karlzhu12@gmail.com>
2023-05-12 09:15:59 +0800

    Rename instance

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,23 @@ fn main() {
 }
 
 fn search_tram_blind() {
-    let my_instance = blind_search::TramProblem::new(17);
+    let tram_problem = blind_search::TramProblem::new(17);
     let mut path: blind_search::Path;
     let mut cost: blind_search::Cost;
 
-    (path, cost) = blind_search::generic_blind_search(&my_instance, blind_search::SearchType::BFS);
+    (path, cost) = blind_search::generic_blind_search(&tram_problem, blind_search::SearchType::BFS);
     println!(
         "The path found by Breadth First Search is {:?}, with time cost {}",
         path, cost
     );
 
-    (path, cost) = blind_search::generic_blind_search(&my_instance, blind_search::SearchType::DFS);
+    (path, cost) = blind_search::generic_blind_search(&tram_problem, blind_search::SearchType::DFS);
     println!(
         "The path found by Depth First Search is {:?}, with time cost {}",
         path, cost
     );
 
-    (path, cost) = blind_search::generic_blind_search(&my_instance, blind_search::SearchType::UCS);
+    (path, cost) = blind_search::generic_blind_search(&tram_problem, blind_search::SearchType::UCS);
     println!(
         "The path found by Uniform Cost Search is {:?}, with time cost {}",
         path, cost
